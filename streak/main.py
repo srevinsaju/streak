36d5244e64f557b2efcad6d44b50b936dee59739
Arghya Bhattacharya <arghya_ab2004@yahoo.in>
2022-03-04 20:31:56 +0530

    Add a callable check for filtering in main

--- streak/main.py
+++ streak/main.py
@@ -7,22 +7,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 default_render_params = {
     "logo": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/313/man-running_1f3c3-200d-2642-fe0f.png",
-    "app_name": "Streak"
+    "app_name": "Streak",
 }
 
 
 for name in filter(lambda a: not a.startswith("_"), dir(api_post)):
-    print(name)
-    app.route(f"/api/v1/{name}", methods=["POST"])(getattr(api_post, name))
+    obj = getattr(api_post, name)
+    if callable(obj):
+        app.route(f"/api/v1/{name}", methods=["POST"])(obj)
 
 for name in filter(lambda a: not a.startswith("_"), dir(api_get)):
-    print(name)
-    app.route(f"/api/v1/{name}")(getattr(api_get, name))
+    obj = getattr(api_get, name)
+    if callable(obj):
+        app.route(f"/api/v1/{name}")(obj)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', **default_render_params)
+    return render_template("index.html", **default_render_params)
+
 
 def main():
     app.run(port=os.getenv("PORT", 5000))
