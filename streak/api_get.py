efb2f5f3fcc125d4747d9b60184d6a3b6d2bbbe5
Arghya Bhattacharya <arghya_ab2004@yahoo.in>
2022-03-04 23:17:13 +0530

    Add some more basic API stuff

--- streak/api_get.py
+++ streak/api_get.py
@@ -1,5 +1,21 @@
-from .api_post import engine
+from . import app
+from .core import utility_funcs
+import uuid
+import json
 
 
-def hello():
-    return "<p>Hello, World!</p>"
+@app.route("/api/v1/tasks/list")
+def list():
+    user_uuid = uuid.UUID("342a8c4a-130a-40b9-a79f-8b784b3b3e24")
+    d = []
+    for task in utility_funcs.get_tasks(user_uuid):
+        d.append(
+            {
+                "id": task.task_id,
+                "name": task.task_name,
+                "description": task.task_description,
+                "schedule": str(task.schedule),
+                "timestamp": str(task.timestamp),
+            }
+        )
+    return json.dumps(d)
--- streak/main.py
+++ streak/main.py
@@ -8,14 +8,8 @@ from . import app
 default_render_params = {
     "logo": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/313/man-running_1f3c3-200d-2642-fe0f.png",
     "app_name": "Streak",
-    "static_url": os.getenv("STATIC_ENDPOINT")
+    "static_url": os.getenv("STATIC_ENDPOINT"),
 }
-
-
-for name in filter(lambda a: not a.startswith("_"), dir(api_get)):
-    obj = getattr(api_get, name)
-    if callable(obj):
-        app.route(f"/api/v1/{name}")(obj)
 
 
 @app.route("/")
