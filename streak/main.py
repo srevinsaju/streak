e05554efe651fdd50e7407ea6cd26228013109a4
Srevin Saju <srevinsaju@sugarlabs.org>
2022-03-04 19:11:51 +0300

    feat: add initial jinja2 templates for UI

--- streak/main.py
+++ streak/main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 default_render_params = {
     "logo": "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/313/man-running_1f3c3-200d-2642-fe0f.png",
     "app_name": "Streak",
+    "static_url": os.getenv("STATIC_ENDPOINT")
 }
 
 
